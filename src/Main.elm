eb538a6cc623e48e966fea3e3afdcb161b9905ff
angusjf <el17ajf@leeds.ac.uk>
2020-02-07 13:17:52 +0100

    moved key into session

--- src/Main.elm
+++ src/Main.elm
@@ -8,11 +8,6 @@ import Pages.Register as Register
 import Session
 import Route
 
-type alias Model =
-  { key : Nav.Key
-  , page : Page
-  }
-
 type Page
   = NotFound Session.Data
   | Register Register.Model
@@ -22,40 +17,36 @@ type Msg
   | UrlChanged Url.Url
   | RegisterMsg Register.Msg
 
-init : () -> Url.Url -> Nav.Key -> (Model, Cmd Msg)
-init _ url key =
- stepUrl url
-   { key = key
-   , page = NotFound Session.empty
-   }
+init : () -> Url.Url -> Nav.Key -> (Page, Cmd Msg)
+init _ url key = stepUrl url (NotFound (Session.fromNavKey key))
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Page -> (Page, Cmd Msg)
 update msg model =
   case msg of
     LinkClicked urlRequest ->
       case urlRequest of
         Browser.Internal url ->
-          (model, Nav.pushUrl model.key (Url.toString url))
+          (model, Nav.pushUrl (getSession model).key (Url.toString url))
         Browser.External href ->
           (model, Nav.load href)
     UrlChanged url ->
       stepUrl url model
     RegisterMsg registerMsg ->
-      case model.page of
+      case model of
         Register registerModel ->
             stepRegister model <| Register.update registerMsg registerModel
         _ ->
             (model, Cmd.none)
 
-stepRegister : Model -> (Register.Model, Cmd Register.Msg) -> (Model, Cmd Msg)
+stepRegister : Page -> (Register.Model, Cmd Register.Msg) -> (Page, Cmd Msg)
 stepRegister model (registerModel, registerCmd) =
-  ( { model | page = Register registerModel }
+  ( Register registerModel
   , Cmd.map RegisterMsg registerCmd
   )
 
-view : Model -> Browser.Document Msg
+view : Page -> Browser.Document Msg
 view model =
-  case model.page of
+  case model of
     NotFound session ->
       { title = "Not Found"
       , body = []
@@ -67,23 +58,23 @@ view model =
           ]
       }
 
-stepUrl : Url.Url -> Model -> (Model, Cmd Msg)
+stepUrl : Url.Url -> Page -> (Page, Cmd Msg)
 stepUrl url model =
   let
     session = getSession model
     route = Route.fromUrl url
   in
     case route of
-      Nothing -> ({ model | page = NotFound session }, Cmd.none)
+      Nothing -> (NotFound session, Cmd.none)
       Just Route.Register -> stepRegister model (Register.init session)
 
-getSession : Model -> Session.Data
+getSession : Page -> Session.Data
 getSession model =
-  case model.page of 
+  case model of 
     NotFound session -> session
     Register registerModel -> registerModel.session
 
-main : Program () Model Msg
+main : Program () Page Msg
 main =
   Browser.application
     { init = init
